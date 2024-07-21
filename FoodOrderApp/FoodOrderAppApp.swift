a4c29cfa161756b85bf80437cead30671309d1f2
Rajat <85779110+rajatDeveloper@users.noreply.github.com>
2024-07-21 20:29:45 +0530

    Initial Commit

---
+++ FoodOrderApp/FoodOrderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodOrderAppApp.swift
+//  FoodOrderApp
+//
+//  Created by RAJAT DHIMAN on 21/07/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodOrderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
